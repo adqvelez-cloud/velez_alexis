8ca36806757841591eff5d80617964e830a3fe59
Alexis Velez <adq.velez@yavirac.edu.ec>
2025-10-22 16:15:28 -0500

    Corregir error intencional en prueba de multiplicación

--- index.test.js
+++ index.test.js
@@ -5,5 +5,5 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('multiplies 2 * 3 to equal 6', () => {
-  expect(multiply(2, 3)).toBe(7); // intentional error
+  expect(multiply(2, 3)).toBe(6);
 });
